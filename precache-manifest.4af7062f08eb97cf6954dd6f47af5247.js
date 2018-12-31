1bb8602c556cec9cb25dda57fcfbec9fc8b9e944
Nick Lils <nicklils@Nicks-Mac-mini.local>
2018-12-31 21:19:48 +0000

    Updates

---
+++ precache-manifest.4af7062f08eb97cf6954dd6f47af5247.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "689ec528c02261ad845c50dc9aa51ea1",
+    "url": "/ViewTube/static/media/samuel-zeller-154708-unsplash.689ec528.jpg"
+  },
+  {
+    "revision": "48cabf655f0d83e2154c",
+    "url": "/ViewTube/static/js/runtime~main.48cabf65.js"
+  },
+  {
+    "revision": "0fb9404d64bd3f02f992",
+    "url": "/ViewTube/static/js/main.0fb9404d.chunk.js"
+  },
+  {
+    "revision": "860f3566b2d17af8e8c6",
+    "url": "/ViewTube/static/js/1.860f3566.chunk.js"
+  },
+  {
+    "revision": "0fb9404d64bd3f02f992",
+    "url": "/ViewTube/static/css/main.a2d189be.chunk.css"
+  },
+  {
+    "revision": "e3fadf89b8ef9a7e2de264ca98bb390f",
+    "url": "/ViewTube/index.html"
+  }
+];
